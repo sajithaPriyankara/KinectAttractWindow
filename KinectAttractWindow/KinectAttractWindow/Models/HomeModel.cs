c65ee3be5caa9900ec67f4212d3950208766a716
Tomasz Kowalczyk <tomek@kownet.info>
2013-04-01 19:00:42 +0200

    trying to view rgb

---
+++ KinectAttractWindow/KinectAttractWindow/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace KinectAttractWindow.Models
+{
+    using System;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Data model for an individual image
+    /// </summary>
+    public class HomeModel
+    {
+        public Image DisplayImage { get; set; }
+    }
+}
--- KinectAttractWindow/KinectAttractWindow/ViewModels/HomeScreenViewModel.cs
+++ KinectAttractWindow/KinectAttractWindow/ViewModels/HomeScreenViewModel.cs
@@ -1,14 +1,61 @@
 ﻿namespace KinectAttractWindow.ViewModels
 {
     using KinectAttractWindow.Navigation;
+    using KinectAttractWindow.Models;
+    using Microsoft.Kinect;
+    using System.Windows.Controls;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Media;
 
     [ExportNavigable(NavigableContextName = DefaultNavigableContexts.HomeScreen)]
     public class HomeScreenViewModel : ViewModelBase
     {
+        public HomeModel RGBImage = new HomeModel();
+        private KinectSensor Sensor;
+
         public HomeScreenViewModel()
             : base()
         {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
 
+                this.Sensor = KinectSensor.KinectSensors[0];
+
+                if (this.Sensor.Status == KinectStatus.Connected)
+                {
+                    this.Sensor.ColorStream.Enable();
+                    this.Sensor.DepthStream.Enable();
+                    this.Sensor.SkeletonStream.Enable();
+                    this.Sensor.AllFramesReady += Sensor_AllFramesReady;
+                    this.Sensor.Start();
+                }
+            }
+        }
+
+        void Sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame == null)
+                {
+                    return;
+                }
+
+                byte[] pixels = new byte[colorFrame.PixelDataLength];
+                colorFrame.CopyPixelDataTo(pixels);
+
+                int stride = colorFrame.Width * 4;
+
+                this.RGBImage.DisplayImage.Source =
+                    BitmapSource.Create(colorFrame.Width,
+                    colorFrame.Height,
+                    96,
+                    96,
+                    PixelFormats.Bgr32,
+                    null,
+                    pixels,
+                    stride);
+            }
         }
     }
 }
